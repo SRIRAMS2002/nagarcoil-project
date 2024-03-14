3a5c9feb06eed36b8d1f5e51a45b4f80a69b2ba8
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2024-03-14 15:51:41 +0530

    agent section 80

--- src/Components/Agent.jsx
+++ src/Components/Agent.jsx
@@ -1,11 +1,348 @@
-import React from 'react'
+import React from "react";
+
+const products = [
+  {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc: "",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  // More products...
+];
 
 const Agent = () => {
   return (
-    <div className='mx-auto max-w-7xl items-center justify-between p-6 lg:px-8'>
-      gg
+    <div className="mx-auto max-w-7xl mt-10  items-center ">
+      <div className="flex">
+  {/* left */}
+  <div>
+    <div className="border-t-2 border-red-500 h-2px mr-[7.2rem] hidden sm:flex"></div>
+    <p className="text-gray-900 mx-5 text-2xl font-bold cursor-pointer flex">
+      FEATURED <span className="text-red-500">AGENTS</span>
+    </p>
+    <p className="text-black/80 mx-5 text-sm mt-1.5 max-w-sm">
+      Meet the opportunity to work with some of the top Real Estate agents in town.
+    </p>
+  </div>
+  {/* right-end */}
+  <div className="justify-end flex items-center">
+    <div className="text-white bg-red-500 rounded-full p-3 my-5">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+      </svg>
     </div>
-  )
-}
 
-export default Agent
+    <div className="mx-2 mt-5">
+      <p className="text-md text-gray-950">view all</p>
+      <p className="text-red-500 text-md font-semibold">Agents</p>
+    </div>
+  </div>
+</div>
+
+
+      {/* Team Section */}
+
+      <div>
+        <div className="mx-auto max-w-7xl mb-[30rem] items-center">
+          <div className="mt-6 px-5 mx-auto grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 overflow-hidden place-content-center place-items-center ">
+            {/* p1 */}
+            <div className="flex flex-col md:flex-row">
+  <div className="md:w-1/3">
+    <img
+      src="https://imgs.search.brave.com/Dj7mxbL3ZB0dPRtSjLiiN9BDwl4iPY-sME1W_01W3_M/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTg1/MTM4NjYwL3Bob3Rv/L3BvcnRyYWl0LW9m/LXNlbmlvci1idXNp/bmVzc21hbi1zbWls/aW5nLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz0zSVh5X21N/emFkcGpjb0wwczhL/cVk3MzZqUU1YQjVK/OVdzaHJFYkJORWZ3/PQ"
+      className="h-52 md:h-full w-full rounded-lg shadow-lg shadow-red-500"
+      alt="Team member"
+    />
+  </div>
+  <div className="md:w-2/3  sm:mt-0">
+    {/* text */}
+    <div>
+                <div>
+                  <h1 className="text-white mt-5 mx-5 max-w-[10rem] bg-red-500 p-1.5 pr-10 rounded-tr-md ">
+                    Amirul feisal
+                  </h1>
+                </div>
+                <div className=" mx-5  w-[16rem]">
+                  <div className="flex py-10  border-r-2 border-t-2 border-red-500">
+                    <div className="flex text-sm items-center mx-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
+                        />
+                      </svg>{" "}
+                      <h1> 25 Listings</h1>
+                    </div>
+                    <div className="flex text-sm mx-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      <h1>REN 1255</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  </div>
+</div>
+
+            {/* p2 */}
+            <div className="flex flex-col md:flex-row">
+  <div className="md:w-1/3">
+    <img
+      src="https://imgs.search.brave.com/Dj7mxbL3ZB0dPRtSjLiiN9BDwl4iPY-sME1W_01W3_M/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTg1/MTM4NjYwL3Bob3Rv/L3BvcnRyYWl0LW9m/LXNlbmlvci1idXNp/bmVzc21hbi1zbWls/aW5nLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz0zSVh5X21N/emFkcGpjb0wwczhL/cVk3MzZqUU1YQjVK/OVdzaHJFYkJORWZ3/PQ"
+      className="h-52 md:h-full w-full rounded-lg shadow-lg shadow-red-500"
+      alt="Team member"
+    />
+  </div>
+  <div className="md:w-2/3  sm:mt-0">
+    {/* text */}
+    <div>
+                <div>
+                  <h1 className="text-white mt-5 mx-5 max-w-[10rem] bg-red-500 p-1.5 pr-10 rounded-tr-md ">
+                    Amirul feisal
+                  </h1>
+                </div>
+                <div className=" mx-5  w-[16rem]">
+                  <div className="flex py-10  border-r-2 border-t-2 border-red-500">
+                    <div className="flex text-sm items-center mx-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
+                        />
+                      </svg>{" "}
+                      <h1> 25 Listings</h1>
+                    </div>
+                    <div className="flex text-sm mx-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      <h1>REN 1255</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  </div>
+</div>
+            {/* p3 */}
+
+            <div className="flex flex-col md:flex-row">
+  <div className="md:w-1/3">
+    <img
+      src="https://imgs.search.brave.com/Dj7mxbL3ZB0dPRtSjLiiN9BDwl4iPY-sME1W_01W3_M/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTg1/MTM4NjYwL3Bob3Rv/L3BvcnRyYWl0LW9m/LXNlbmlvci1idXNp/bmVzc21hbi1zbWls/aW5nLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz0zSVh5X21N/emFkcGpjb0wwczhL/cVk3MzZqUU1YQjVK/OVdzaHJFYkJORWZ3/PQ"
+      className="h-52 md:h-full w-full rounded-lg shadow-lg shadow-red-500"
+      alt="Team member"
+    />
+  </div>
+  <div className="md:w-2/3  sm:mt-0">
+    {/* text */}
+    <div>
+                <div>
+                  <h1 className="text-white mt-5 mx-5 max-w-[10rem] bg-red-500 p-1.5 pr-10 rounded-tr-md ">
+                    Amirul feisal
+                  </h1>
+                </div>
+                <div className=" mx-5  w-[16rem]">
+                  <div className="flex py-10  border-r-2 border-t-2 border-red-500">
+                    <div className="flex text-sm items-center mx-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
+                        />
+                      </svg>{" "}
+                      <h1> 25 Listings</h1>
+                    </div>
+                    <div className="flex text-sm mx-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      <h1>REN 1255</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  </div>
+</div>
+
+            {/* p4 */}
+            <div className="flex flex-col md:flex-row">
+  <div className="md:w-1/3">
+    <img
+      src="https://imgs.search.brave.com/Dj7mxbL3ZB0dPRtSjLiiN9BDwl4iPY-sME1W_01W3_M/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTg1/MTM4NjYwL3Bob3Rv/L3BvcnRyYWl0LW9m/LXNlbmlvci1idXNp/bmVzc21hbi1zbWls/aW5nLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz0zSVh5X21N/emFkcGpjb0wwczhL/cVk3MzZqUU1YQjVK/OVdzaHJFYkJORWZ3/PQ"
+      className="h-52 md:h-full w-full rounded-lg shadow-lg shadow-red-500"
+      alt="Team member"
+    />
+  </div>
+  <div className="md:w-2/3  sm:mt-0">
+    {/* text */}
+    <div>
+                <div>
+                  <h1 className="text-white mt-5 mx-5 max-w-[10rem] bg-red-500 p-1.5 pr-10 rounded-tr-md ">
+                    Amirul feisal
+                  </h1>
+                </div>
+                <div className=" mx-5  w-[16rem]">
+                  <div className="flex py-10  border-r-2 border-t-2 border-red-500">
+                    <div className="flex text-sm items-center mx-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
+                        />
+                      </svg>{" "}
+                      <h1> 25 Listings</h1>
+                    </div>
+                    <div className="flex text-sm mx-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      <h1>REN 1255</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  </div>
+</div>
+            {/* p5 */}
+            <div className="flex flex-col md:flex-row">
+  <div className="md:w-1/3">
+    <img
+      src="https://imgs.search.brave.com/Dj7mxbL3ZB0dPRtSjLiiN9BDwl4iPY-sME1W_01W3_M/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTg1/MTM4NjYwL3Bob3Rv/L3BvcnRyYWl0LW9m/LXNlbmlvci1idXNp/bmVzc21hbi1zbWls/aW5nLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz0zSVh5X21N/emFkcGpjb0wwczhL/cVk3MzZqUU1YQjVK/OVdzaHJFYkJORWZ3/PQ"
+      className="h-52 md:h-full w-full rounded-lg shadow-lg shadow-red-500"
+      alt="Team member"
+    />
+  </div>
+  <div className="md:w-2/3  sm:mt-0">
+    {/* text */}
+    <div>
+                <div>
+                  <h1 className="text-white mt-5 mx-5 max-w-[10rem] bg-red-500 p-1.5 pr-10 rounded-tr-md ">
+                    Amirul feisal
+                  </h1>
+                </div>
+                <div className=" mx-5  w-[16rem]">
+                  <div className="flex py-10  border-r-2 border-t-2 border-red-500">
+                    <div className="flex text-sm items-center mx-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
+                        />
+                      </svg>{" "}
+                      <h1> 25 Listings</h1>
+                    </div>
+                    <div className="flex text-sm mx-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      <h1>REN 1255</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  </div>
+</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Agent;
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import {
-    MagnifyingGlassIcon,
-  } from '@heroicons/react/24/outline'
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 const Hero = () => {
   return (
     <div
@@ -9,74 +7,99 @@ const Hero = () => {
       aria-label="Global"
     >
       <div className="grid grid-cols-1 gap-0 sm:grid-cols-2 ">
-   {/* left side */}
-   <div>
-        {/* tab */}
-        <div className="flex space-x-10">
-          <div>
-            <h1 className="text-red-500 text-[28px] font-bold ">850+</h1>
-            <p>Property</p>
-            <p>Listings</p>
+        {/* left side */}
+        <div>
+          {/* tab */}
+          <div className="flex space-x-10">
+            <div>
+              <h1 className="text-red-500 text-[28px] font-bold ">850+</h1>
+              <p>Property</p>
+              <p>Listings</p>
+            </div>
+            <div>
+              <h1 className="text-red-500 text-[28px] font-bold ">110+</h1>
+              <p>Esteemed</p>
+              <p>Agents</p>
+            </div>
+            <div>
+              <h1 className="text-red-500 text-[28px] font-bold ">958+</h1>
+              <p>Regular</p>
+              <p>Clients</p>
+            </div>
           </div>
-          <div>
-            <h1 className="text-red-500 text-[28px] font-bold ">110+</h1>
-            <p>Esteemed</p>
-            <p>Agents</p>
+          {/* content */}
+          <div className="relative   bg-black w-[19.5rem] h-[18rem] max-w-none rounded-se-[3rem]  shadow-xl ring-1 ring-gray-400/10 sm:w-[30rem] sm:h-[20rem] mt-5 md:-ml-4 lg:-ml-0">
+            <h1 className="relative text-white max-w-sm mx-5 font-bold text-2xl md:leading-10 pt-8 md:text-[40px]">
+              CREATING BLISSFUL PROPERTY SEARCH IS OUR SPECIALITY
+            </h1>
+            <p className="text-white/80 mx-5 text-sm mt-3.5 max-w-sm">
+              with a lot of experience we will help you shortlist the Properties
+              you want
+            </p>
+
+            {/* input */}
+            <div>
+              <div className="relative mt-2 max-w-lg  ml-5">
+                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                  <span className="text-gray-500 ">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                      />
+                    </svg>
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  name="price"
+                  id="price"
+                  className="block w-[16.5rem] sm:w-[32rem] bg-gray-200 mt-5 h-12 sm:h-16 z-10 border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Search by project,street name, area"
+                />
+                <div className="absolute inset-y-0 right-0 sm:-right-10 flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+                    />
+                  </svg>
+
+                  <button className="bg-red-600 p-2 text-white ml-3">
+                    search
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <h1 className="text-red-500 text-[28px] font-bold ">958+</h1>
-            <p>Regular</p>
-            <p>Clients</p>
-          </div>
         </div>
-        {/* content */}
-        <div className="relative   bg-black w-[19.5rem] h-[18rem] max-w-none rounded-se-[3rem]  shadow-xl ring-1 ring-gray-400/10 sm:w-[30rem] sm:h-[20rem] mt-5 md:-ml-4 lg:-ml-0">
-          <h1 className="relative text-white max-w-sm mx-5 font-bold text-2xl md:leading-10 pt-8 md:text-[40px]">CREATING BLISSFUL PROPERTY SEARCH IS OUR SPECIALITY</h1>
-          <p className="text-white/80 mx-5 text-sm mt-3.5 max-w-sm">with a lot of experience we will help you shortlist the Properties you want</p>
 
-          {/* input */}
-          <div>
-
-      <div className="relative mt-2 max-w-lg  ml-5">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <span className="text-gray-500 "><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-</svg>
-</span>
+        <div className=" hidden sm:flex space-x-60 relative">
+          <img
+            src="/Hero-1.jpg"
+            alt=""
+            className="h-[28.1rem] w-[18rem] absolute inset-0  -left-20 -z-10"
+          />
+          <img src="/Hero-1.jpg" alt="h-[28.1rem]  " />
         </div>
-        <input
-          type="text"
-          name="price"
-          id="price"
-          className="block w-[16.5rem] sm:w-[30rem] bg-gray-200 mt-5 h-12 sm:h-16 z-10 border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          placeholder="Search by project,street name, area"
-        />
-        <div className="absolute inset-y-0 right-0 sm:-right-10 flex items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
-</svg>
-
-<button className="bg-red-600 p-2 text-white ml-3">
-    search
-</button>
-
-        </div>
+        <div></div>
       </div>
-    </div>
-        </div>
-      </div>
-
-      <div className=" hidden sm:flex space-x-60 relative">
-        <img src="/Hero-1.jpg" alt="" className="h-[28.1rem] w-[18rem] absolute inset-0  -left-20 -z-10" />
-        <img src="/Hero-1.jpg" alt="h-[28.1rem]  " />
-        
-      </div>
-      <div>
-      
-      </div>
-      </div>
-   
-
     </div>
   );
 };
