fe689377857a1f97fc915a2e968ace405f9f0225
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2024-03-14 08:33:54 +0530

    height-ui

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -30,8 +30,8 @@ const Hero = () => {
           </div>
         </div>
         {/* content */}
-        <div className="relative   bg-black w-[18rem] h-[16rem] max-w-none rounded-se-[3rem]  shadow-xl ring-1 ring-gray-400/10 sm:w-[27rem] sm:h-[20rem] mt-5 md:-ml-4 lg:-ml-0">
-          <h1 className="relative text-white max-w-sm mx-5 font-bold text-2xl md:leading-10 md:mt-10 mt-5 md:text-[40px]">CREATING BLISSFUL PROPERTY SEARCH IS OUR SPECIALITY</h1>
+        <div className="relative   bg-black w-[18rem] h-[18rem] max-w-none rounded-se-[3rem]  shadow-xl ring-1 ring-gray-400/10 sm:w-[27rem] sm:h-[20rem] mt-5 md:-ml-4 lg:-ml-0">
+          <h1 className="relative text-white max-w-sm mx-5 font-bold text-2xl md:leading-10 pt-8 md:text-[40px]">CREATING BLISSFUL PROPERTY SEARCH IS OUR SPECIALITY</h1>
           <p className="text-white/80 mx-5 text-sm mt-3.5 max-w-sm">with a lot of experience we will help you shortlist the Properties you want</p>
 
           {/* input */}
